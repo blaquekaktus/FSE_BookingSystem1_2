863723a5006e81a6b384f950cdc78806a1c9644d
Sonja Lechner <blaquekaktus@gmail.com>
2023-02-12 01:24:14 +0100

    Booking Class update

--- src/main/java/com/itkolleg/bookingsystem/Domains/Booking.java
+++ src/main/java/com/itkolleg/bookingsystem/Domains/Booking.java
@@ -1,4 +1,39 @@
 package com.itkolleg.bookingsystem.Domains;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    //@JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+
+    private Desk desk;
+
+    private Date bookingStart;
+    private Date bookingEnd;
+    private Timestamp bookingTime;
+
+    public Booking(User user, Desk desk, Date bookingStart, Date bookingEnd, Timestamp bookingTime){
+        this.user=user;
+        this.desk=desk;
+        this.bookingStart=bookingStart;
+        this.bookingEnd=bookingEnd;
+        this.bookingTime=bookingTime;
+    }
 }
