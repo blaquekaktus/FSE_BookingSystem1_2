79dacf21a02dac3b7859e09ce5f0a12be8a01b62
Sonja Lechner <blaquekaktus@gmail.com>
2023-02-12 03:35:09 +0100

    Deleted PortJPARepo Ports are not an entity but are embedded objects which are a part of the desk entity.

--- src/main/java/com/itkolleg/bookingsystem/Repos/PortJPARepo.java
+++
@@ -1,7 +0,0 @@
-package com.itkolleg.bookingsystem.Repos;
-
-import com.itkolleg.bookingsystem.Domains.Port;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PortJPARepo extends JpaRepository<Port, Long> {
-}
