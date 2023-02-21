39b69277c246c3d086aacbe24ecab72ed5fecf47
Sonja Lechner <blaquekaktus@gmail.com>
2023-02-21 23:28:03 +0100

    DeskBooking Repo

--- src/main/java/com/itkolleg/bookingsystem/Repos/DeskBookings/DBAccessDeskBookingJPAH2.java
+++ src/main/java/com/itkolleg/bookingsystem/Repos/DeskBookings/DBAccessDeskBookingJPAH2.java
@@ -1,8 +1,7 @@
 package com.itkolleg.bookingsystem.Repos.DeskBookings;
 
-import com.itkolleg.bookingsystem.Domains.Booking;
+import com.itkolleg.bookingsystem.Domains.Booking.DeskBooking;
 import com.itkolleg.bookingsystem.Domains.Desk;
-import com.itkolleg.bookingsystem.Domains.DeskBooking;
 import com.itkolleg.bookingsystem.Domains.Employee;
 import com.itkolleg.bookingsystem.Exceptions.DeskExeceptions.DeskNotFoundException;
 import com.itkolleg.bookingsystem.Exceptions.EmployeeExceptions.EmployeeNotFoundException;
@@ -13,34 +12,48 @@ import java.util.List;
 
 @Component
 public class DBAccessDeskBookingJPAH2 implements DBAccessDeskBookings {
-
     DeskBookingJPARepo deskBookingJPARepo;
 
     public DBAccessDeskBookingJPAH2(DeskBookingJPARepo deskBookingJPARepo) {
         this.deskBookingJPARepo = deskBookingJPARepo;
     }
 
-
     @Override
     public DeskBooking createDeskBooking(DeskBooking deskBooking) {
-        return null;
+        return this.deskBookingJPARepo.save(deskBooking);
     }
-
 
     @Override
     public List<DeskBooking> getAllDeskBookings() {
-        return null;
+        List<DeskBooking> deskBookingList = this.deskBookingJPARepo.findAll();
+        for (DeskBooking deskBooking : deskBookingList){
+            System.out.println(deskBooking);
+        }
+        return  this.deskBookingJPARepo.findAll();
     }
 
     @Override
     public List<DeskBooking> getDeskBookingsByEmployee(Employee employee) {
+        return this.deskBookingJPARepo.getByEmployee(employee);
+    }
+
+    /**
+     * @param deskID
+     * @return
+     */
+    @Override
+    public List<DeskBooking> getDeskBookingsByDeskID(Long deskID) {
         return null;
     }
 
-
     @Override
     public List<DeskBooking> getDeskBookingsByEmployeeID(Long employeeID) {
-        return null;
+        return this.deskBookingJPARepo.getDeskBookingsByEmployeeID(employeeID);
+    }
+
+    @Override
+    public List<DeskBooking> getDeskBookingsByDesk(Desk desk) {
+        return this.deskBookingJPARepo.getByDesk(desk);
     }
 
     /**
@@ -50,19 +63,7 @@ public class DBAccessDeskBookingJPAH2 implements DBAccessDeskBookings {
      */
     @Override
     public List<DeskBooking> getDeskBookingsByDeskId(Long deskID) throws DeskNotFoundException {
-        return null;
-    }
-
-
-    @Override
-    public List<DeskBooking> getDeskBookingsByDeskID(Long deskID) {
-        return null;
-    }
-
-
-    @Override
-    public List<DeskBooking> getDeskBookingsByDesk(Desk desk) {
-        return null;
+        return this.deskBookingJPARepo.getDeskBookingsByDeskID(deskID);
     }
 
     /**
@@ -72,43 +73,40 @@ public class DBAccessDeskBookingJPAH2 implements DBAccessDeskBookings {
      */
     @Override
     public DeskBooking updateDeskBookingByEmployeeId(Long employeeID) throws EmployeeNotFoundException {
-        return null;
+        return this.deskBookingJPARepo.updateDeskBookingByEmployeeId(employeeID);
     }
 
     /**
-     * @param DeskID
+     * @param deskID
      * @return
      * @throws EmployeeNotFoundException
      */
     @Override
-    public DeskBooking updateDeskBookingByDeskId(Long DeskID) throws EmployeeNotFoundException {
-        return null;
+    public DeskBooking updateDeskBookingByDeskId(Long deskID) throws EmployeeNotFoundException {
+        return this.deskBookingJPARepo.updateDeskBookingByDeskId(deskID);
     }
 
+    /**
+     * @param deskBooking
+     * @return
+     */
     @Override
     public DeskBooking updateDeskBooking(DeskBooking deskBooking) {
         return null;
     }
 
+    @Override
+    public void deleteDeskBookingByDeskId(Long deskID) {
+        this.deskBookingJPARepo.deleteDeskBookingByDeskId(deskID);
+    }
 
     @Override
     public void deleteDeskBookingByEmployeeId(Long employeeID) {
-
+        this.deskBookingJPARepo.deleteDeskBookingByEmployeeId(employeeID);
     }
 
 
-    @Override
-    public void deleteDeskBookingByDeskId(Long DeskID) {
 
-    }
 
-    /**
-     * @param desk
-     * @return
-     */
-    @Override
-    public List<Booking> getAllDeskBookingsByDesk(Desk desk) {
-        return null;
-    }
 
 }
--- src/main/java/com/itkolleg/bookingsystem/Repos/DeskBookings/DeskBookingJPARepo.java
+++ src/main/java/com/itkolleg/bookingsystem/Repos/DeskBookings/DeskBookingJPARepo.java
@@ -1,13 +1,32 @@
 package com.itkolleg.bookingsystem.Repos.DeskBookings;
 
-import com.itkolleg.bookingsystem.Domains.Booking;
+import com.itkolleg.bookingsystem.Domains.Booking.Booking;
+import com.itkolleg.bookingsystem.Domains.Booking.DeskBooking;
+import com.itkolleg.bookingsystem.Domains.Desk;
+import com.itkolleg.bookingsystem.Domains.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  This interface provides CRUD operations and additional methods for accessing and manipulating {@link Booking} data
  from the database using Spring Data JPA. It extends the {@link JpaRepository} interface.
  */
 @Repository
-public interface DeskBookingJPARepo extends JpaRepository<Booking, Long> {
+public interface DeskBookingJPARepo extends JpaRepository<DeskBooking, Long> {
+    List<DeskBooking>getByEmployee(Employee employee);
+    List<DeskBooking>getByDesk(Desk desk);
+
+    List<DeskBooking>getDeskBookingsByEmployeeID(Long employeeID);
+
+    List<DeskBooking>getDeskBookingsByDeskID(Long deskID);
+
+    DeskBooking updateDeskBookingByEmployeeId(Long employeeID);
+
+    DeskBooking updateDeskBookingByDeskId(Long deskID);
+
+    void deleteDeskBookingByEmployeeId(Long employeeID);
+
+    void deleteDeskBookingByDeskId(Long deskID);
 }
